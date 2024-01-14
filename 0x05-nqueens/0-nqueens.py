497cc49c27cc869c79d6010195c60949b2a2790a
papaskripto <bygeoffreyouma@gmail.com>
2024-01-14 16:12:40 +0300

    Add solution to the nqueens problem

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -19,3 +19,43 @@ except ValueError:
 if n < 4:
     print("N must be at least 4")
     exit(1)
+
+def is_row_safe(board, row, col, n):
+    """ Check if the queen is in the same row.
+    """
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+def nqueens_solution(board, col, n):
+    if col == n:
+        print_sol(board, n)
+        return
+
+    for i in range(n):
+        if is_row_safe(board, i, col, n): 
+            board[i][col] = 1
+            nqueens_solution(board, col + 1, n)
+
+            board[i][col] = 0
+
+def solve_nqueens(n):
+    board = [[0] * n for _ in range(n)]
+
+    nqueens_solution(board, 0, n)
+
+def print_sol(board, n):
+    for i in range(n):
+        for j in range(n):
+            print(board[i][j], end=" ")
+        print()
