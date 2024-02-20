5050634a4ef06e665a6b92fcfea2013e03800016
papaskripto <bygeoffreyouma@gmail.com>
2024-02-20 13:03:15 +0300

    Update main file

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,11 +3,8 @@
 Prime Game.
 '''
 
-
 def findMultiples(num, targets):
-    """
-    Finds multiples of a given number within a list
-    """
+    ''' Finds multiples of numbers.'''
     for i in targets:
         if i % num == 0:
             targets.remove(i)
@@ -15,9 +12,7 @@ def findMultiples(num, targets):
 
 
 def isPrime(i):
-    """
-    Check if a number is prime.
-    """
+    ''' Check if a number is prime or not.'''
     if i == 1:
         return False
     for j in range(2, i):
@@ -27,9 +22,7 @@ def isPrime(i):
 
 
 def findPrimes(n):
-    """
-    Dispatch a given set into prime numbers and non-prime numbers.
-    """
+    ''' Determine whethere a number is prime or not.'''
     counter = 0
     target = list(n)
     for i in range(1, len(target) + 1):
@@ -43,22 +36,12 @@ def findPrimes(n):
 
 
 def isWinner(x, nums):
-    """
-    Maria and Ben are playing a game.Given a set of consecutive integers
-    starting from 1 up to and including n, they take turns choosing a
-    prime number from the set and removing that number and its
-    multiples from the set.
-    The player that cannot make a move loses the game.
-
-    They play x rounds of the game, where n may be different for each round.
-    Assuming Maria always goes first and both players play optimally,
-    determine who the winner of each game is.
-    """
+    ''' Find the winner. '''
     players = {'Maria': 0, 'Ben': 0}
     cluster = set()
-    for elem in range(x):
+    for element in range(x):
         nums.sort()
-        num = nums[elem]
+        num = nums[element]
         for i in range(1, num + 1):
             cluster.add(i)
             if i == num + 1:
