7f82a5a77b3494e39056d8d9b5aedba623971e79
skriptomaven <bygeoffreyouma@gmail.com>
2024-05-07 09:58:54 +0300

    Touch technically empty files

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+Island perimeter module.
+'''
+
+
+def island_perimeter(grid):
+    '''
+    Returns the perimeter of the island described in grid.
+    '''
+    grid = []
+    cells 
+    return perimeter
