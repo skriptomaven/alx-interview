df3c035d83adca541e04730d2bbbb3b7e75e57cf
skriptomaven <bygeoffreyouma@gmail.com>
2024-05-08 08:39:47 +0300

    Redo this

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,6 +8,16 @@ def island_perimeter(grid):
     '''
     Returns the perimeter of the island described in grid.
     '''
-    grid = []
-    cells 
+    perimeter = 0
+    for i, j in enumerate(grid):
+        for k, cell in enumerate(j):
+            if cell == 1:
+                if i == 0 or grid[i - 1][k] == 0:
+                    perimeter += 1
+                if i == len(grid) - 1 or grid[i + 1][k] == 0:
+                    perimeter += 1
+                if k == 0 or grid[i][k - 1] == 0:
+                    perimeter += 1
+                if k == len(j) - 1 or grid[i][k + 1] == 0:
+                    perimeter += 1
     return perimeter
